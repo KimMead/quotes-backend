52c7b38b15ccf98f57c516a34cd180205c847bc8
KimMead <kimmeadows8@gmail.com>
2021-05-30 16:19:50 -0500

    add quotes and categories controllers

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,2 +1,37 @@
 class Api::V1::CategoriesController < ApplicationController
+    
+    def index
+        @categories = Categories.all
+        render json: @categories 
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save 
+            render json: @category
+        else 
+            render json: {error: 'Error creating category'}
+        end
+    end 
+
+    def show
+        @category = Category.find(params[:id])
+        render json: @category
+    end
+
+    def destroy
+        @category = Category.find(params[:id])
+        @category.destroy 
+    end 
+
+    def edit
+
+    end 
+
+    private
+
+    def category_params
+        params.require(:category).permit(:name)
+        
+    end 
 end
--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -1,3 +1,41 @@
 class Api::V1::QuotesController < ApplicationController
    
+    before_action :set_category
+    
+    def index
+        @quotes = @category.quotes
+        render json: @quotes
+    end
+
+    def show
+        # @quote = @category.quotes.find_by(id: params[:id])
+        @quote = Quote.find(params[:id])
+        render json: @quote
+    end
+
+    def create
+       @quote = @category.quotes.new(quote_params)
+       @quote.save 
+       render json: @quote
+    end 
+
+    def destroy
+         
+    end 
+
+    def edit
+
+    end 
+
+    private
+
+    def set_category
+        @category = Category.find(params[:category_id])
+    end 
+
+
+    def quote_params
+        params.require(:quote).permit(:category_id, :author, :content, :description)
+        
+    end 
 end
